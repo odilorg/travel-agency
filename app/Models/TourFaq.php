4f516014acc42ac4df6c684b416c8f9ba499c341
odil <odilorg@gmail.com>
2025-10-06 23:28:11 +0500

    fix(models): add fillable/translatable and relations for inclusion/exclusion/itinerary/faq

--- app/Models/TourFaq.php
+++ app/Models/TourFaq.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TourFaq extends Model
 {
-    //
+    use HasTranslations;
+
+    protected $fillable = [
+        'tour_id',
+        'position',
+        'question',
+        'question_translations',
+        'answer_html',
+        'answer_html_translations',
+    ];
+
+    public array $translatable = ['question','answer_html'];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
--- app/Models/TourItineraryItem.php
+++ app/Models/TourItineraryItem.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TourItineraryItem extends Model
 {
-    //
+    use HasTranslations;
+
+    protected $fillable = [
+        'tour_id',
+        'position',
+        'day',
+        'time',
+        'title',
+        'title_translations',
+        'body_html',
+        'body_html_translations',
+    ];
+
+    public array $translatable = ['title','body_html'];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
