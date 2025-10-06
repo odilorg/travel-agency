615195cc391f220562d3809bee1d98e38b862462
odil <odilorg@gmail.com>
2025-10-06 23:37:01 +0500

    fix(models): add fillable/casts and tour() relation to TourReview

--- app/Models/TourReview.php
+++ app/Models/TourReview.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class TourReview extends Model
 {
-    //
+    protected $fillable = [
+        'tour_id',
+        'author_name',
+        'author_email',
+        'rating',
+        'title',
+        'body',
+        'verified_booking',
+        'approved',
+    ];
+
+    protected $casts = [
+        'verified_booking' => 'boolean',
+        'approved' => 'boolean',
+    ];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
