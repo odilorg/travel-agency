f805537294a0b0ff522cf6182f7c4b7fd8a589bf
odil <odilorg@gmail.com>
2025-10-06 23:26:14 +0500

    fix(models): add fillable/translatable and relation to TourHighlight

--- app/Models/TourHighlight.php
+++ app/Models/TourHighlight.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TourHighlight extends Model
 {
-    //
+    use HasTranslations;
+
+    protected $fillable = [
+        'tour_id',
+        'position',
+        'label',
+        'label_translations',
+    ];
+
+    public array $translatable = ['label'];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
