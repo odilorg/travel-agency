f24c3fe03cf56dfb9481d28cc9c22945eaf5eefe
odil <odilorg@gmail.com>
2025-10-06 23:23:31 +0500

    fix(models): add fillable/translatable/casts and relation to TourPriceOption

--- app/Models/TourPriceOption.php
+++ app/Models/TourPriceOption.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TourPriceOption extends Model
 {
-    //
+    use HasTranslations;
+
+    protected $fillable = [
+        'tour_id',
+        'position',
+        'name',
+        'name_translations',
+        'price',
+        'currency',
+        'min_pax',
+        'max_pax',
+        'is_active',
+    ];
+
+    public array $translatable = ['name'];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
