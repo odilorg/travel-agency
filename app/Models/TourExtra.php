1afe198a7335536e524efc06ff5e3d0b4ed4dcbb
odil <odilorg@gmail.com>
2025-10-06 23:24:55 +0500

    fix(models): add fillable/translatable/casts and relation to TourExtra

--- app/Models/TourExtra.php
+++ app/Models/TourExtra.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class TourExtra extends Model
 {
-    //
+    use HasTranslations;
+
+    protected $fillable = [
+        'tour_id',
+        'position',
+        'label',
+        'label_translations',
+        'description',
+        'description_translations',
+        'price',
+        'per_person',
+    ];
+
+    public array $translatable = ['label','description'];
+
+    protected $casts = [
+        'per_person' => 'boolean',
+    ];
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
 }
